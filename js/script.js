5c9e4fcbd960671f966518fc4404f9421974e9da
BuildTools <xxredstonetekxx@gmail.com>
2020-03-10 00:06:44 -0400

    Sun bass reactivity

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ const TERRAIN_GENERATION_SPEED = 20;
 /* Vue.js Configuration */
 
 var app;
-var isFrequency = false;
 
 function initializeVue() {
     app = new Vue({
@@ -22,24 +21,8 @@ function initializeVue() {
             },
             toggleAudio: function(e) {
                 if(this.paused) {
-                    if(source !== undefined) {
-                        var remainingUntilBeat = 60000.0 / bpm - (pauseTime - lastBeat);
-                        setTimeout(() => {
-                            graphicsBeat();
-                            lastBeat = Date.now();
-                            beatId = setInterval(() => {
-                                if(this.paused) return;
-                                graphicsBeat();
-                                lastBeat = Date.now();
-                            }, 60000.0 / bpm);
-                        }, remainingUntilBeat);
-                    }
-
                     audioCtx.resume();
                 } else {
-                    pauseTime = Date.now();
-                    clearInterval(beatId);
-
                     audioCtx.suspend();
                 }
                 this.paused = !this.paused;
@@ -59,54 +42,53 @@ function initializeVue() {
 
 /* Audio Playback */
 
-var audioCtx, gainNode, analyserNode, source;
-var dataArray, bpm, beatId, lastBeat, pauseTime;
+var audioCtx, gainNode, sources, filterLFNode, analyserLFNode, filterHFNode, analyserHFNode;
+var dataArray;
 
 function initializeAudio() {
     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    analyserNode = audioCtx.createAnalyser();
     gainNode = audioCtx.createGain();
-
-    analyserNode.connect(gainNode);
     gainNode.connect(audioCtx.destination);
 
-    analyserNode.fftSize = 2048;
-    var bufferLength = analyserNode.frequencyBinCount;
+    filterLFNode = audioCtx.createBiquadFilter();
+    analyserLFNode = audioCtx.createAnalyser();
+
+    filterLFNode.connect(analyserLFNode);
+
+    filterLFNode.type = "lowpass";
+    filterLFNode.frequency.setValueAtTime(250, audioCtx.currentTime);
+    filterLFNode.gain.setValueAtTime(25, audioCtx.currentTime);
+
+    analyserLFNode.fftSize = 2048;
+    var bufferLength = analyserLFNode.frequencyBinCount;
     dataArray = new Uint8Array(bufferLength);
 }
 
 function loadAudio(event) {
-    if(source !== undefined) source.stop(0);
-    if(beatId !== undefined) clearInterval(beatId);
+    if(sources !== undefined) sources.forEach((source) => source.stop(0));
 
-    source = audioCtx.createBufferSource();
+    sources = new Array(2);
+    for(var i = 0; i < sources.length; i++) {
+        sources[i] = audioCtx.createBufferSource();
+    }
     
     var file = event.target.files[0];
     var reader = new FileReader();
     reader.onload = (e) => {
         var buffer = e.target.result;
         audioCtx.decodeAudioData(buffer, (audioBuffer) => {
-            webAudioBeatDetector.analyze(audioBuffer)
-                .then((tempo) => {
-                    source.buffer = audioBuffer;
-                    source.connect(analyserNode);
-                    source.start(0);
-                    source.onended = (e) => {
-                        source = undefined;
-                        clearInterval(beatId);
-                    };
-
-                    bpm = tempo;
-                    graphicsBeat();
-                    lastBeat = Date.now();
-                    beatId = setInterval(() => {
-                        graphicsBeat();
-                        lastBeat = Date.now();
-                    }, 60000.0 / bpm);
-                })
-                .catch((err) => {
-                    console.log(`Error while determining BPM. ${err}`);
-                });
+            for(var i = 0; i < sources.length; i++) {
+                sources[i].buffer = audioBuffer;
+                switch(i) {
+                    case 0:
+                        sources[i].connect(gainNode);
+                        break;
+                    case 1:
+                        sources[i].connect(filterLFNode);
+                        break;
+                }
+                sources[i].start(0);
+            }
         });
     };
     reader.readAsArrayBuffer(file);
@@ -115,7 +97,7 @@ function loadAudio(event) {
 /* Three.js Visualizer */
 
 var perlin, scene, camera, renderer, width, height;
-var terrain, tick;
+var terrain, sun, sunCenter, tick;
 
 function initializeThree() {
     perlin = new Perlin(Date.now());
@@ -144,6 +126,13 @@ function initializeThree() {
     terrain.position.y = -50;
     scene.add(terrain);
 
+    var sunGeometry = new THREE.CircleGeometry(50, 32);
+    var sunMaterial = new THREE.MeshBasicMaterial({color: 0xFFEA00});
+    sun = new THREE.Mesh(sunGeometry, sunMaterial);
+    sun.position.z = -125;
+    sun.position.y = 80;
+    scene.add(sun);
+
     var ambientLight = new THREE.AmbientLight(0x303030);
     scene.add(ambientLight);
 
@@ -156,20 +145,37 @@ function initializeThree() {
     animateThree();
 }
 
+function getHeightAtDepth(depth) {
+    var openingAngle = camera.fov * Math.PI / 180;
+    return 2 * Math.tan(openingAngle / 2) * depth;
+}
+
+function getWidthAtDepth(depth) {
+    return getHeightAtDepth(depth) * camera.aspect;
+}
+
 function resizeThree() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    var openingAngle = camera.fov * Math.PI / 180;
-    height = 2 * Math.tan(openingAngle / 2) * CAMERA_DISTANCE;
-    width = height * camera.aspect;
+    height = getHeightAtDepth(CAMERA_DISTANCE);
+    width = getWidthAtDepth(CAMERA_DISTANCE);
 }
 
 function animateThree() {
     requestAnimationFrame(animateThree);
 
-    tick++;
+    analyserLFNode.getByteTimeDomainData(dataArray);
+    var highestLFSample = 0;
+    for(var i = 0; i < dataArray.length; i++) {
+        if(dataArray[i] > highestLFSample) highestLFSample = dataArray[i];
+    }
+    var bass = (highestLFSample - 128) / 128;
+    var value = Math.max(sun.scale.x - 0.025, 1 + bass);
+    sun.scale.x = value;
+    sun.scale.y = value;
 
+    if(audioCtx.state != "suspended" && sources !== undefined) tick++;
     if(tick % (60 / TERRAIN_GENERATION_SPEED) == 0) {
         for(var x = 0; x < TERRAIN_SIZE; x++) {
             for(var y = 0; y < TERRAIN_SIZE; y++) {
@@ -179,14 +185,11 @@ function animateThree() {
                           0);
             }
         }
+        terrain.material.color.setHSL((tick / (60 * 15)) % 1, 1.0, 0.6);
         terrain.geometry.verticesNeedUpdate = true;
     }
 
     renderer.render(scene, camera);
-}
-
-function graphicsBeat() {
-    /* DO SOMETHING WITH BEAT, OR MAYBE USE A SINE WAVE TO INTERPOLATE AN EFFECT WITH THE GIVEN FREQUENCY? */
 }
 
 /* Setting Callbacks */
